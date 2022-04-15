14ec010054657912840d14286fbddd4333de20d2
jpapazian2000 <jpapazian@hashicorp.com>
2022-04-15 17:50:02 +0200

    adding first files

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "gcp region"
+  default     = "europe-west1"
+}git a
+
+variable "zone" {
+  description = "gcp zone"
+  default     = "europe-west1-b"
+}
+
+variable "project" {
+  description = "gcp project"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
