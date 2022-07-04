4cb6ee5c81992e25554a2e20b3d510c6e2ce7f83
jerome <j_papazian2000@yahoo.fr>
2022-07-04 15:07:09 +0200

    adaption of code

--- variables.tf
+++ variables.tf
@@ -1,15 +1,33 @@
 variable "region" {
   description = "gcp region"
-  default     = "europe-west1"
+  default     = "europe-west3"
 }
 
 variable "zone" {
   description = "gcp zone"
+  default = "europe-west3-c"
 }
 
 variable "project" {
   description = "gcp project"
 }
+variable "owner" { }
+variable "se-region" {}
+variable "purpose" {}
+variable "ttl" {}
+variable "terraform" {}
+variable "hc-internet-facing" {}
+variable "prefix" {}
 
+variable "subnet_prefix" {}
+
+variable "machine_type" {}
+
+variable "ssh_keys" {}
+
+variable "ssh_user" {
+    default = "jerome"
+}
+variable "allowed_ip" {}
 
 
